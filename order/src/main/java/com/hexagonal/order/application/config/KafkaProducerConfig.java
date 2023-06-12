c0e353c6c6ba8f1738aa93c5d6242449caadc2a1
dlwodud821@gmail.com <dlwodud821@gmail.com>
2023-06-12 23:57:26 +0900

    refactor: Product Kafka Consumer Deserializer 의존성 참조 수정

--- order/src/main/java/com/hexagonal/order/application/config/KafkaProducerConfig.java
+++ order/src/main/java/com/hexagonal/order/application/config/KafkaProducerConfig.java
@@ -1,4 +1,4 @@
-package com.hexagonal.product.application.config;
+package com.hexagonal.order.application.config;
 
 import com.fasterxml.jackson.databind.ser.std.MapSerializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -11,22 +11,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @EnableKafka
-public class KafkaConfig {
+public class KafkaProducerConfig {
 
-	@Value("${spring.kafka.bootstrap-servers}")
-	private String bootstrapServers;
+	@Value("${spring.kafka.producer.bootstrap-servers}")
+	private String producerBootstrapServer;
+
 
 	@Bean
 	public ProducerFactory<String, String> producerFactory() {
 		Map<String, Object> configProps = new HashMap<>();
-		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, producerBootstrapServer);
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		return new DefaultKafkaProducerFactory<>(configProps);
@@ -35,10 +35,9 @@ public class KafkaConfig {
 	@Bean
 	public ProducerFactory<String, Map<Long, Long>> producerProductFactory() {
 		Map<String, Object> configProps = new HashMap<>();
-		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, producerBootstrapServer);
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, MapSerializer.class);
-
 		return new DefaultKafkaProducerFactory<>(configProps);
 	}
 
@@ -47,31 +46,8 @@ public class KafkaConfig {
 		return new KafkaTemplate<>(producerFactory());
 	}
 
-
-	@Bean
-	public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-		ConcurrentKafkaListenerContainerFactory<String, String> factory =
-				new ConcurrentKafkaListenerContainerFactory<>();
-		factory.setConsumerFactory(consumerFactory());
-		return factory;
-	}
-
-	public Map<String, Object> consumerConfigs() {
-		Map<String, Object> props = new HashMap<>();
-		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-		props.put(ConsumerConfig.GROUP_ID_CONFIG, "product-group");
-		return props;
-	}
-
-	public ConsumerFactory<String, String> consumerFactory() {
-		return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-	}
-
 	@Bean
 	public KafkaTemplate<String, Map<Long, Long>> kafkaProductInventoryTemplate(){
 		return new KafkaTemplate<>(producerProductFactory());
 	}
 }
-
--- product/src/main/java/com/hexagonal/product/application/config/ProducerKafkaConfig.java
+++ product/src/main/java/com/hexagonal/product/application/config/ProducerKafkaConfig.java
@@ -1,32 +1,28 @@
-package com.hexagonal.order.application.config;
+package com.hexagonal.product.application.config;
 
 import com.fasterxml.jackson.databind.ser.std.MapSerializer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @EnableKafka
-public class KafkaConfig {
+public class ProducerKafkaConfig {
 
 	@Value("${spring.kafka.bootstrap-servers}")
-	private String bootstrapServers;
+	private String bootstrapServer;
 
 	@Bean
 	public ProducerFactory<String, String> producerFactory() {
 		Map<String, Object> configProps = new HashMap<>();
-		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		return new DefaultKafkaProducerFactory<>(configProps);
@@ -35,9 +31,10 @@ public class KafkaConfig {
 	@Bean
 	public ProducerFactory<String, Map<Long, Long>> producerProductFactory() {
 		Map<String, Object> configProps = new HashMap<>();
-		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, MapSerializer.class);
+
 		return new DefaultKafkaProducerFactory<>(configProps);
 	}
 
@@ -47,28 +44,8 @@ public class KafkaConfig {
 	}
 
 	@Bean
-	public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-		ConcurrentKafkaListenerContainerFactory<String, String> factory =
-				new ConcurrentKafkaListenerContainerFactory<>();
-		factory.setConsumerFactory(consumerFactory());
-		return factory;
-	}
-
-	public Map<String, Object> consumerConfigs() {
-		Map<String, Object> props = new HashMap<>();
-		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-		props.put(ConsumerConfig.GROUP_ID_CONFIG, "order-group");
-		return props;
-	}
-
-	public ConsumerFactory<String, String> consumerFactory() {
-		return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-	}
-
-	@Bean
 	public KafkaTemplate<String, Map<Long, Long>> kafkaProductInventoryTemplate(){
 		return new KafkaTemplate<>(producerProductFactory());
 	}
 }
+
